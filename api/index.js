1a96f8f44a3fa3d8d4effea2cebb727a4499585d
Roberto Ibacache <robertoibacache@Robertos-MacBook-Pro.local>
2025-04-08 16:07:01 -0400

    fix: removed googleapis import

--- api/index.js
+++ api/index.js
@@ -1,6 +1,3 @@
-
-const { google } = require("googleapis");
-
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Método no permitido" });
